cad74762073e7f49e24853ab5371cdb6945ef3a7
Kelsey Z <you@example.comzahandke@gmail.com>
2017-11-02 17:51:15 -0400

    home link
    added home link on Z

--- wp-content/themes/Zahandtheme/header.php
+++ wp-content/themes/Zahandtheme/header.php
@@ -16,7 +16,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Z</a>
+      <a class="navbar-brand" href=" <?php echo home_url(); ?> ">Z</a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
